74ec4452b6beb47e7ea1f041d1662321e3baf072
Vinoth Ramiah <vin-git.1u18t@simplelogin.com>
2023-12-19 17:30:07 +0000

    add delay to share function in FakeShareRepository

--- lib/src/features/share/data/fake_share_repository.dart
+++ lib/src/features/share/data/fake_share_repository.dart
@@ -1,9 +1,17 @@
 import 'package:bouser/src/features/share/data/share_repository.dart';
+import 'package:bouser/src/utils/delay.dart';
 import 'package:flutter/foundation.dart';
 
 class FakeShareRepository implements ShareRepository {
+  FakeShareRepository({
+    this.addDelay = true,
+  });
+
+  final bool addDelay;
+
   @override
   Future<void> share(String url) async {
+    await delay(addDelay);
     debugPrint('Sharing $url');
   }
 }
