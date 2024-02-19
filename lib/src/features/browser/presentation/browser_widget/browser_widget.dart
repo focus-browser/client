1fb222ca8f1a33c63119bbbc5e24e4a1f1df8d7d
Vinoth Ramiah <vin-git.1u18t@simplelogin.com>
2024-02-19 21:43:48 +0000

    make webview transparent when current url is null

--- lib/src/features/browser/presentation/browser_widget/browser_widget.dart
+++ lib/src/features/browser/presentation/browser_widget/browser_widget.dart
@@ -1,8 +1,9 @@
-import 'package:focus_browser/src/features/browser/data/inappwebview_browser_repository/inappwebview_browser_repository.dart';
-import 'package:focus_browser/src/features/browser/presentation/browser_screen/browser_screen_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:focus_browser/src/features/browser/data/browser_repository.dart';
+import 'package:focus_browser/src/features/browser/data/inappwebview_browser_repository/inappwebview_browser_repository.dart';
+import 'package:focus_browser/src/features/browser/presentation/browser_screen/browser_screen_controller.dart';
 
 final browserWidgetNumberProvider = Provider.autoDispose<int>((ref) {
   throw UnimplementedError();
@@ -16,34 +17,38 @@ class BrowserWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final browserNumber = ref.watch(browserWidgetNumberProvider);
+    final currentUrlIsEmpty =
+        ref.watch(browserCurrentUrlProvider(browserNumber)).value == null;
     return Listener(
       onPointerDown: (_) => ref
           .read(browserScreenControllerProvider.notifier)
           .setSelectedBrowser(browserNumber),
-      child: InAppWebView(
-        initialOptions: InAppWebViewGroupOptions(
-          crossPlatform: InAppWebViewOptions(
-            transparentBackground: true,
-            incognito: true,
+      child: Opacity(
+        opacity: currentUrlIsEmpty ? 0.0 : 1.0,
+        child: InAppWebView(
+          initialOptions: InAppWebViewGroupOptions(
+            crossPlatform: InAppWebViewOptions(
+              incognito: true,
+            ),
+            ios: IOSInAppWebViewOptions(
+              maximumZoomScale: 5.0,
+            ),
           ),
-          ios: IOSInAppWebViewOptions(
-            maximumZoomScale: 5.0,
-          ),
+          onWebViewCreated: (webViewController) => ref
+              .read(inAppWebViewBrowserRepositoryProvider)
+              .setController(browserNumber, webViewController),
+          onLoadStart: (controller, url) => ref
+              .read(inAppWebViewBrowserRepositoryProvider)
+              .updateCurrentUrl(browserNumber, url),
+          onLoadStop: (controller, url) => ref
+              .read(inAppWebViewBrowserRepositoryProvider)
+              .updateCurrentUrl(browserNumber, url),
+          onLoadError: (controller, url, code, message) => debugPrint(
+              "browserNumber: $browserNumber, url: $url, code: $code, message: $message"),
+          onUpdateVisitedHistory: (controller, url, androidIsReload) => ref
+              .read(inAppWebViewBrowserRepositoryProvider)
+              .updateCurrentUrl(browserNumber, url),
         ),
-        onWebViewCreated: (webViewController) => ref
-            .read(inAppWebViewBrowserRepositoryProvider)
-            .setController(browserNumber, webViewController),
-        onLoadStart: (controller, url) => ref
-            .read(inAppWebViewBrowserRepositoryProvider)
-            .updateCurrentUrl(browserNumber, url),
-        onLoadStop: (controller, url) => ref
-            .read(inAppWebViewBrowserRepositoryProvider)
-            .updateCurrentUrl(browserNumber, url),
-        onLoadError: (controller, url, code, message) => debugPrint(
-            "browserNumber: $browserNumber, url: $url, code: $code, message: $message"),
-        onUpdateVisitedHistory: (controller, url, androidIsReload) => ref
-            .read(inAppWebViewBrowserRepositoryProvider)
-            .updateCurrentUrl(browserNumber, url),
       ),
     );
   }
