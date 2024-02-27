056e072eea0ba004053949f0d19ad169a2c83ada
Vinoth Ramiah <vin-git.1u18t@simplelogin.com>
2024-02-27 17:26:27 +0000

    disable debug logs from flutter_inappwebview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focus_browser/src/app.dart';
@@ -44,6 +45,7 @@ void main() async {
   );
 
   WidgetsFlutterBinding.ensureInitialized();
+  PlatformInAppWebViewController.debugLoggingSettings.enabled = false;
 
   final searchEnginesRepository =
       await SembastSearchEnginesRepository.makeDefault();
